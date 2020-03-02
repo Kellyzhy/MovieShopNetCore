d1778d508e329de309ecd05eefbaa3db40e3cba2
Kellyzhy <46937935+Kellyzhy@users.noreply.github.com>
2020-03-02 16:48:29 -0500

    MovieShopNetCore

---
+++ MovieShop.API/Controllers/GenresController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShop.API.Controllers
+{
+    [Route("api/[controller]")] //routing
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAllGenres()
+        {
+            //create a new anonymous array
+            var genres = new[]
+                          {
+                             new {Id = 1, Name = "Action"},
+                             new {Id = 2, Name = "Comedy"},
+                             new {Id = 3, Name = "Thriller"}
+                         };
+            return Ok(genres);
+        }
+    }
+}
